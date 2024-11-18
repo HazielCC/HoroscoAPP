03f35fd0d057295fd9287ea165574eb6ca3e18d0
Haziel Covarrubias <67761409+HazielCC@users.noreply.github.com>
2024-11-17 20:46:39 -0700

    Agregar nuevos fragmentos y archivos de configuración para la aplicación Horosco APP

--- app/src/main/java/com/example/horoscoapp/ui/home/MainActivity.kt
+++ app/src/main/java/com/example/horoscoapp/ui/home/MainActivity.kt
@@ -1,16 +1,22 @@
-package com.example.horoscoapp
+package com.example.horoscoapp.ui.home
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.horoscoapp.R
+import com.example.horoscoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
