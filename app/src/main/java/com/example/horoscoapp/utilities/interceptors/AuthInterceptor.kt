c69fa939666e8faefd0ad209065caeae3c02e9e5
Haziel Covarrubias <67761409+HazielCC@users.noreply.github.com>
2024-12-20 15:53:57 -0700

    Añadir AuthInterceptor para manejar la autenticación en las solicitudes de red.

---
+++ app/src/main/java/com/example/horoscoapp/utilities/interceptors/AuthInterceptor.kt
@@ -0,0 +1,10 @@
+package com.example.horoscoapp.utilities.interceptors
+
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class AuthIterceptor : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        TODO("Not yet implemented")
+    }
+}
