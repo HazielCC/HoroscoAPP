6398d9f14ca82698cd19092c138f513ae6435a51
Haziel Covarrubias <67761409+HazielCC@users.noreply.github.com>
2024-12-24 11:03:31 -0700

    ADD: Agregar permisos de cámara y mejorar la gestión de permisos en PalmistryFragment

--- app/src/main/java/com/example/horoscoapp/ui/palmistry/PalmistryFragment.kt
+++ app/src/main/java/com/example/horoscoapp/ui/palmistry/PalmistryFragment.kt
@@ -1,17 +1,27 @@
 package com.example.horoscoapp.ui.palmistry
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.horoscoapp.databinding.FragmentPalmistryBinding
+import com.example.horoscoapp.utilities.permissions.PermissionsService
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class PalmistryFragment : Fragment() {
     private var _binding: FragmentPalmistryBinding? = null
     private val binding get() = _binding!!
+
+    // Utilities
+    private val tag = "PalmistryFragment"
+
+    @Inject
+    lateinit var permissionsService: PermissionsService
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,6 +29,13 @@ class PalmistryFragment : Fragment() {
     ): View {
         _binding = FragmentPalmistryBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Log.d(tag, permissionsService.checkCameraPermission().toString())
+
     }
 
     override fun onDestroyView() {
