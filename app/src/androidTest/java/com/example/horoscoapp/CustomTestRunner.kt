cb1ece560c7b5fa164b8356baddeba494b96fe5c
Haziel Covarrubias <67761409+HazielCC@users.noreply.github.com>
2024-12-27 12:39:15 -0700

    ADD: Agregar clases de prueba personalizadas para mejorar la estructura de pruebas en la aplicación

---
+++ app/src/androidTest/java/com/example/horoscoapp/CustomTestRunner.kt
@@ -0,0 +1,4 @@
+package com.example.horoscoapp
+
+class CustomTestRunner {
+}
---
+++ app/src/androidTest/java/com/example/horoscoapp/ui/home/MainActivityTest.kt
@@ -0,0 +1,5 @@
+package com.example.horoscoapp.ui.home
+
+import org.junit.jupiter.api.Assertions.*
+
+class MainActivityTest
